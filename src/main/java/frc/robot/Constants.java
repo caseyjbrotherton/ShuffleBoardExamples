c9dbbcddecd20eee6edb9917451a61f5877aa9c1
Casey Brotherton <caseyjbrotherton@gmail.com>
2022-01-19 12:28:02 -0600

    Create simple dashboard examples

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,37 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    // Enum example from: 
+    // https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+    public enum Planet {
+        MERCURY (3.303e+23, 2.4397e6),
+        VENUS   (4.869e+24, 6.0518e6),
+        EARTH   (5.976e+24, 6.37814e6),
+        MARS    (6.421e+23, 3.3972e6),
+        JUPITER (1.9e+27,   7.1492e7),
+        SATURN  (5.688e+26, 6.0268e7),
+        URANUS  (8.686e+25, 2.5559e7),
+        NEPTUNE (1.024e+26, 2.4746e7);
+    
+        private final double mass;   // in kilograms
+        private final double radius; // in meters
+        Planet(double mass, double radius) {
+            this.mass = mass;
+            this.radius = radius;
+        }
+        private double mass() { return mass; }
+        private double radius() { return radius; }
+    
+        // universal gravitational constant  (m3 kg-1 s-2)
+        public static final double G = 6.67300E-11;
+    
+        double surfaceGravity() {
+            return G * mass / (radius * radius);
+        }
+        double surfaceWeight(double otherMass) {
+            return otherMass * surfaceGravity();
+        }
+    }; // end of Planet
+}
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,14 +5,79 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+
+import java.util.Map;
 
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  /** Using this example subsystem to explore some ShuffleBoard Code examples.
+   * 
+   *  Shuffleboard can display Doubles, Boolean values as well as strings.
+   *  In addition, if there are objects that implement Sendable, those can also be used
+   *  ( Commands are all sendable. )
+  **/
 
+  // Double will change periodically
+  private double exampleDouble = 0.0;
+  // ExampleTruth will be true if example Double is even, or if explicitly set.
+  private boolean exampleTruth = true;
+  // exampleChange will change without the driver's control based on current time.
+  private double exampleChange = 0;
+  private long startMillis = System.currentTimeMillis();
+
+  private ShuffleboardTab exTab;
+  private NetworkTableEntry sbExampleDouble;
+  private NetworkTableEntry sbExampleTruth;
+  private NetworkTableEntry sbExampleChange;
+
+  public ExampleSubsystem() {
+    // Shuffleboard can have multiple tabs.  
+    // getTab will create a new tab, or return an existing one.
+    // Multiple subsystems can call getTab and send widgets to the same tab.
+    exTab = Shuffleboard.getTab("Random Layout");
+    // The following widgets are very simple. Automatic placement, default widgets.
+    sbExampleDouble = exTab.add("Example Double",exampleDouble).getEntry();
+    sbExampleTruth = exTab.add("Example Truth", exampleTruth).getEntry();
+
+    // Possible Widgets are all here:  
+    // https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/shuffleboard/BuiltInWidgets.html
+    // This is a read only number bar.
+    sbExampleChange = exTab
+    .add("Example Changes", 0.0)
+    .withWidget(BuiltInWidgets.kNumberBar)
+    .withProperties(Map.of("min",0,"max",60))
+    .getEntry();
+
+  }
+
+  public double getExampleDouble(){
+    return exampleDouble;
+  }
+
+  public void setExampleDouble(double d){
+    // If we know when the data in the variable changes, we can update the widget at the same time.
+    exampleDouble=d;
+    sbExampleDouble.setDouble(exampleDouble);
+
+  }
+
+  public boolean getExampleTruth(){
+    return exampleTruth;
+  }
+  public void setExampleTruth(boolean t){
+    // If we know when the data in the variable changes, we can update the widget at the same time.
+    exampleTruth=t;
+    sbExampleTruth.setBoolean(t);
+  }
+ 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Count from 0-60 and back over again for each minute the code has been running.
+    exampleChange=((System.currentTimeMillis()-startMillis)/1000) % 60;
+    // If we don't control when the data might change, we can poll and add those changes to the dashboard.
+    sbExampleChange.setDouble(exampleChange);
+
   }
 
   @Override
